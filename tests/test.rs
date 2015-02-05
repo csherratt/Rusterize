dd1123fb5ae4d2a9ed762765b401392d8045e20e
Colin Sherratt <colin.sherratt@gmail.com>
2015-02-05 01:47:11 -0500

    expanded tests

--- tests/test.rs
+++ tests/test.rs
@@ -1,3 +1,5 @@
+#![feature(path)]
+
 extern crate image;
 extern crate genmesh;
 extern crate cgmath;
@@ -10,16 +12,17 @@ use genmesh::{Triangulate, MapToVertices};
 use std::old_io::File;
 use image::Rgb;
 
-fn save(name: &str, img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>) {
-    // Save the image as "fractal.png"
-    let fout = File::create(&Path::new(name)).unwrap();
+fn check(name: &str, img: image::ImageBuffer<image::Rgb<u8>, Vec<u8>>) {
+    // Save the image output just incase the test fails
+    let fout = File::create(&Path::new("test_data/results").join(Path::new(name))).unwrap();
+    let _= image::ImageRgb8(img.clone()).save(fout, image::PNG);
 
-    //We must indicate the image's color type and what format to save as.
-    let _= image::ImageRgb8(img).save(fout, image::PNG);
+    let expected = image::open(&Path::new("test_data/expected").join(Path::new(name))).unwrap();
+    assert!(expected.raw_pixels() == img.as_slice());
 }
 
 #[test]
-fn test_plane() {
+fn plane() {
     let mut frame = Frame::new(16, 16);
     let cube = generators::Plane::new()
         .triangulate()
@@ -29,15 +32,11 @@ fn test_plane() {
         Rgb([255, 255, 255])
     });
 
-    save("test_data/results/plane.png", frame.frame.clone());
-
-    let img = image::open(&Path::new("test_data/expected/plane.png")).unwrap();
-    assert!(img.raw_pixels() == frame.frame.as_slice());
-
+    check("plane.png", frame.frame.clone());
 }
 
 #[test]
-fn test_plane_backface() {
+fn plane_backface() {
     let mut frame = Frame::new(16, 16);
     let cube = generators::Plane::new()
         .triangulate()
@@ -47,14 +46,11 @@ fn test_plane_backface() {
         Rgb([255, 255, 255])
     });
 
-    save("test_data/results/plane_backface.png", frame.frame.clone());
-
-    let img = image::open(&Path::new("test_data/expected/plane_backface.png")).unwrap();
-    assert!(img.raw_pixels() == frame.frame.as_slice());
+    check("plane_backface.png", frame.frame.clone());
 }
 
 #[test]
-fn test_plane_fill() {
+fn plane_fill() {
     let mut frame = Frame::new(16, 16);
     let cube = generators::Plane::new()
         .triangulate()
@@ -64,14 +60,11 @@ fn test_plane_fill() {
         Rgb([255, 255, 255])
     });
 
-    save("test_data/results/plane_fill.png", frame.frame.clone());
-
-    let img = image::open(&Path::new("test_data/expected/plane.png")).unwrap();
-    assert!(img.raw_pixels() == frame.frame.as_slice());
+    check("plane_fill.png", frame.frame.clone());
 }
 
 #[test]
-fn test_plane_overfill() {
+fn plane_overfill() {
     let mut frame = Frame::new(16, 16);
     let cube = generators::Plane::new()
         .triangulate()
@@ -81,8 +74,5 @@ fn test_plane_overfill() {
         Rgb([255, 255, 255])
     });
 
-    save("test_data/results/plane_overfill.png", frame.frame.clone());
-
-    let img = image::open(&Path::new("test_data/expected/plane.png")).unwrap();
-    assert!(img.raw_pixels() == frame.frame.as_slice());
+    check("plane_overfill.png", frame.frame.clone());
 }
