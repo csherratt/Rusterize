600edbed578a36174549aa56078ab63a9a069de1
Colin Sherratt <colin.sherratt@gmail.com>
2015-02-07 01:55:59 -0500

    add monkey based benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,10 +1,11 @@
-#![feature(test)]
+#![feature(test, path)]
 
 extern crate image;
 extern crate genmesh;
 extern crate cgmath;
 extern crate rusterize;
 extern crate test;
+extern crate obj;
 
 use rusterize::Frame;
 use cgmath::*;
@@ -88,3 +89,28 @@ fn plane_back_front(bench: &mut Bencher) {
     });
 }
 
+#[bench]
+fn monkey(bench: &mut Bencher) {
+    let obj = obj::load(&Path::new("test_assets/monkey.obj")).unwrap();
+    let monkey = obj.object_iter().next().unwrap().group_iter().next().unwrap();
+
+    let proj = ortho(-1.5, 1.5, -1.5, 1.5, -10., 10.);
+
+    let light_normal = Vector4::new(10., 10., 10., 0.).normalize();
+    let kd = Vector4::new(64., 128., 64., 1.);
+    let ka = Vector4::new(16., 16., 16., 1.);
+
+    bench.iter(|| {
+        let vertex = monkey.indices().iter().map(|x| *x)
+                           .vertex(|(p, _, n)| { (obj.position()[p], obj.normal()[n.unwrap()]) })
+                           .vertex(|(p, n)| (proj.mul_v(&Vector4::new(p[0], p[1], p[2], 1.)).into_fixed(), n))
+                           .triangulate();
+
+        let mut frame = Frame::new(SIZE, SIZE);
+        frame.raster(vertex, |(_, n)| {
+            let normal = Vector4::new(n[0], n[1], n[2], 0.);
+            let v = kd.mul_s(light_normal.dot(&normal).partial_max(0.))  + ka;
+            Rgb([v.x as u8, v.y as u8, v.z as u8])
+        });
+    });
+}
